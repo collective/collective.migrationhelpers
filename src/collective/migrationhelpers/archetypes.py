9a3976f7069a3a1100a57b0bdad38f0cf1de404d
Philip Bauer <bauer@starzel.de>
2020-07-17 10:07:06 +0200

    fix __parent__ of conversations after migrating from AT to DX

--- src/collective/migrationhelpers/archetypes.py
+++ src/collective/migrationhelpers/archetypes.py
@@ -36,3 +36,24 @@ def remove_archetypes(context=None):
                 log.info('Deleted {}'.format(tool))
             except Exception as e:
                 log.info(u'Another problem removing {}: {}'.format(tool, e))
+
+
+def fix_conversations(context=None):
+    """Conversations from plone.app.discussion may still have the old
+    Archetypes content as __parent__ instead of the new migrated DX-content.
+    # TODO: Fix in plone.app.contenttypes?
+    """
+    from Acquisition import aq_base
+    portal = api.portal.get()
+
+    def fix_conversation_parent(obj, path):
+        annotations = getattr(aq_base(obj), '__annotations__', None)
+        if not annotations:
+            return
+        if 'plone.app.discussion:conversation' not in annotations.keys():
+            return
+        conversation = annotations['plone.app.discussion:conversation']
+        if 'broken' in conversation.__parent__.__repr__() or obj != conversation.__parent__:
+            conversation.__parent__ = obj
+            log.info(u'Fix conversation for {}'.format(obj.absolute_url()))
+    portal.ZopeFindAndApply(portal, search_sub=True, apply_func=fix_conversation_parent)
