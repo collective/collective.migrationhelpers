9a854d344a21cc7220c579bfb8f1a6ac4496b64d
Maurits van Rees <maurits@vanrees.org>
2021-03-23 16:32:15 +0100

    fix_at_image_scales: look in all rich text fields.
    Needs more testing, as my sample content did not contain any rich text with AT images.

--- src/collective/migrationhelpers/images.py
+++ src/collective/migrationhelpers/images.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 from plone import api
+from plone.app.textfield.interfaces import IRichText
 from plone.app.textfield.value import RichTextValue
+from plone.dexterity.utils import iterSchemata
+from zope.schema import getFieldsInOrder
 
 import logging
 
@@ -53,30 +56,43 @@ def fix_at_image_scales(context=None):
     """
     catalog = api.portal.get_tool('portal_catalog')
     query = {}
-    query['object_provides'] = 'plone.app.contenttypes.behaviors.richtext.IRichText'  # noqa: E501
-    results = catalog.unrestrictedSearchResults(**query)
-    log.info('There are {0} items with rich text behavior in total, starting migration...'.format(
-        len(results)))
+    if hasattr(catalog, "getAllBrains"):
+        results = catalog.getAllBrains()
+    else:
+        results = catalog.unrestrictedSearchResults(**query)
+    log.info('Starting migration of image scales in rich text fields.')
     for result in results:
         try:
             obj = result.getObject()
-        except:
+        except (KeyError, AttributeError):
             log.warning(
                 'Not possible to fetch object from catalog result for '
                 'item: {0}.'.format(result.getPath()))
             continue
-
-        text = getattr(obj.aq_base, 'text', None)
-        if text:
-            clean_text = image_scale_fixer(text.raw)
-            if clean_text != text.raw:
-                obj.text = RichTextValue(
+        changed = False
+        for schema in iterSchemata(obj):
+            fields = getFieldsInOrder(schema)
+            for name, field in fields:
+                if not IRichText.providedBy(field):
+                    continue
+                text = getattr(obj.aq_base, name, None)
+                if not text:
+                    continue
+                clean_text = image_scale_fixer(text.raw)
+                if clean_text == text.raw:
+                    continue
+                setattr(obj, name, RichTextValue(
                     raw=clean_text,
                     mimeType=text.mimeType,
                     outputMimeType=text.outputMimeType,
                     encoding=text.encoding
-                )
-                obj.reindexObject(idxs=('SearchableText', ))
-                log.info('Text cleanup for {0}'.format(
-                    '/'.join(obj.getPhysicalPath())
                 ))
+                changed = True
+                log.info('Text cleanup in field {0} for {1}'.format(
+                    name, '/'.join(obj.getPhysicalPath())
+                ))
+        if changed:
+            obj.reindexObject(idxs=('SearchableText', ))
+            log.info('Text cleanup for {0}'.format(
+                '/'.join(obj.getPhysicalPath())
+            ))
